142bc1d8607bfa008cbddcf1205648ab4722a948
Leo Lou <leo.lou@gov.bc.ca>
2016-02-04 21:06:02 -0800

    Create runme.js

---
+++ runme.js
@@ -0,0 +1,30 @@
+var restify = require('restify')
+var apistatus = require('apistatus')
+var server = restify.createServer()
+
+server.use(restify.queryParser())
+server.get('/', function (req, res, next) {
+
+  var url = req.params.url
+
+  if (!url) {
+    res.send(400, {error: "missing target url"})
+    next()
+    return
+  }
+
+  apistatus(url, function(status){
+    res.send(status)
+    next()
+  })
+
+})
+
+server.get('/ping', function (req, res, next) {
+  res.send("pong")
+  next()
+})
+
+server.listen(4080, function() {
+  console.log('%s listening at %s', server.name, server.url)
+})
